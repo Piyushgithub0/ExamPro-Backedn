4b8ec75bdf9e349d2c71835a5a395a9ffbe65a9b
Piyush Patil <piyushjpatil46@gmail.com>
2026-04-07 23:38:18 +0530

    Deployment Ready

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cors({
   origin: [
     'http://localhost:5173',
     'http://localhost:3000',
-    'https://your-frontend.vercel.app' // 🔥 replace later
+    'https://evalix-smart-examination.vercel.app/' // 🔥 replace later
   ],
   credentials: true,
 }));
